6750836af0cf2148363914a8161f26de0b4a3b41
mackwick <mackenziewicker@gmail.com>
2024-03-22 12:07:44 -0400

    Commit: CARMEN SANDIEGO - Halfway there! Hot on her trail.

---
+++ carmen_sandiego.sql
@@ -0,0 +1,63 @@
+-- ADDED STARTER CODE from world.sql
+
+CREATE TABLE city (
+    id integer NOT NULL,
+    name text NOT NULL,
+    countrycode character(3) NOT NULL,
+    district text NOT NULL,
+    population integer NOT NULL
+);
+
+CREATE TABLE country (
+    code character(3) NOT NULL,
+    name text NOT NULL,
+    continent text NOT NULL,
+    region text NOT NULL,
+    surfacearea real NOT NULL,
+    indepyear smallint,
+    population integer NOT NULL,
+    lifeexpectancy real,
+    gnp numeric(10,2),
+    gnpold numeric(10,2),
+    localname text NOT NULL,
+    governmentform text NOT NULL,
+    headofstate text,
+    capital integer,
+    code2 character(2) NOT NULL,
+    CONSTRAINT country_continent_check CHECK ((((((((continent = 'Asia'::text) OR (continent = 'Europe'::text)) OR (continent = 'North America'::text)) OR (continent = 'Africa'::text)) OR (continent = 'Oceania'::text)) OR (continent = 'Antarctica'::text)) OR (continent = 'South America'::text)))
+);
+
+CREATE TABLE countrylanguage (
+    countrycode character(3) NOT NULL,
+    "language" text NOT NULL,
+    isofficial boolean NOT NULL,
+    percentage real NOT NULL
+);
+
+-------------------------------------------------------------
+--Find the least populated country in Southern Europe
+-------------------------------------------------------------
+-- Get sum of the city populations with shared country code where the country code matches and the region is 'Southern Europe'
+
+-- SELECT city.name, city.population, city.countrycode FROM city INNER JOIN country ON city.countrycode = country.code WHERE region = 'Southern Europe'
+-- It's The Vatican (countrycode VAT)
+
+-------------------------------------------------------------
+-- Find the country's official language
+-------------------------------------------------------------
+-- SELECT language, isofficial FROM countrylanguage WHERE countrycode = 'VAT'
+-- It's Italian
+
+-------------------------------------------------------------
+-- Nearby that country only speaks that language
+-------------------------------------------------------------
+-- SELECT * FROM countrylanguage WHERE language = 'Italian' AND percentage = 100
+--SMR only speaks Italian
+-- SELECT * FROM country WHERE code = 'SMR'
+-- It's San Marino
+
+-------------------------------------------------------------
+-- City in San Marino that is not the same name as the country
+-------------------------------------------------------------
+-- SELECT * FROM city WHERE countrycode = 'SMR' AND name != 'San Marino'
+-- It's Serravalle
