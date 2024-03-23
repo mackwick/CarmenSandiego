dc2d758c2d9df9afd29c9ec9455febf8d1b2137d
mackwick <mackenziewicker@gmail.com>
2024-03-23 16:39:36 -0400

    NFL db seeded

---
+++ NFL.sql
@@ -0,0 +1,18 @@
+create table teams(
+	id serial primary key,
+	name varchar(255) not null,
+	stadium varchar(255),
+	division varchar(255),
+	conference varchar(255),
+	head_coach varchar(255),
+	active boolean
+);
+
+create table players(
+	id serial primary key,
+	name varchar(255) not null,
+	position varchar(255),
+	salary integer,
+	team_id integer references teams
+);
+
