5cfa57bf5c243942d6d768ce0c266fc0098ba50f
mackwick <mackenziewicker@gmail.com>
2024-03-23 16:46:33 -0400

    NFL queries 1

--- NFL.sql
+++ NFL.sql
@@ -16,3 +16,23 @@ create table players(
 	team_id integer references teams
 );
 
+-------------------------------------------------------------
+-- 1. List the names of all NFL teams
+-------------------------------------------------------------
+SELECT name FROM teams
+
+-------------------------------------------------------------
+-- 2. List the stadium name and head coach of all NFC teams
+-------------------------------------------------------------
+SELECT stadium, head_coach FROM teams
+
+-------------------------------------------------------------
+-- 3. List the head coaches of the AFC South
+-------------------------------------------------------------
+SELECT head_coach FROM teams WHERE conference = 'AFC' AND division = 'South'
+
+-------------------------------------------------------------
+-- 4. The total number of players in the NFL
+-------------------------------------------------------------
+SELECT * FROM players 
+Answer: 1532 (there are that many rows)
