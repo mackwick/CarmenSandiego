b300c739de96d0c08f96ba8633fd08d03f333566
mackwick <mackenziewicker@gmail.com>
2024-03-23 16:54:51 -0400

    NFL queries 2

--- NFL.sql
+++ NFL.sql
@@ -36,3 +36,26 @@ SELECT head_coach FROM teams WHERE conference = 'AFC' AND division = 'South'
 -------------------------------------------------------------
 SELECT * FROM players 
 Answer: 1532 (there are that many rows)
+
+-------------------------------------------------------------
+-- 5. The team names and head coaches of the NFC North and AFC East
+-------------------------------------------------------------
+SELECT name, head_coach, FROM teams WHERE conference = 'NFC' AND division = 'North' OR conference = 'AFC' AND division = 'East'
+
+-------------------------------------------------------------
+-- 6. The 50 players with the highest salaries
+-------------------------------------------------------------
+SELECT * FROM players ORDER BY salary DESC LIMIT 50
+
+-------------------------------------------------------------
+-- 7. The average salary of all NFL players
+-------------------------------------------------------------
+SELECT AVG(salary) FROM players
+Answer: $1,579,692.54
+
+-------------------------------------------------------------
+-- 8. The names and positions of players with a salary above 10_000_000
+-------------------------------------------------------------
+SELECT name, position FROM players WHERE salary > 10000000
+
+
